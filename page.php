ff5c824c6d4ca022bd5965d453be05299291d7ab
M3fjuLeo <mateuszropek@onet.pl>
2025-02-13 12:30:40 +0100

    blog continued

--- page.php
+++ page.php
@@ -16,6 +16,7 @@ get_header();
 
     
     <div class="container container--narrow page-section">
+      
       <?php 
       $theParent = wp_get_post_parent_id(get_the_ID());
       if($theParent){ ?>
