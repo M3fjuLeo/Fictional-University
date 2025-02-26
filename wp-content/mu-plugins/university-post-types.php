a781eeee18ca302480cff512a1b8a7b8f93503ad
M3fjuLeo <mateuszropek@onet.pl>
2025-02-26 19:50:57 +0100

    Manipulating Default URL Based Queries

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -2,6 +2,8 @@
  
 function university_post_types() {
   register_post_type('event', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug'=> 'events'),
     'has_archive' => true,
     'public' => true,
@@ -14,7 +16,7 @@ function university_post_types() {
       'singular_name' => 'Event'
     ),
     'menu_icon' => 'dashicons-calendar'
-  ));
+  )); 
 }
  
 add_action('init', 'university_post_types');
